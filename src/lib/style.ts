9ea01c71fc9f22cf9924a2d2ab8cb3f7cdd4cf82
Pierre K. <OzPerle@users.noreply.github.com>
2025-06-14 08:03:23 +1000

    Update style - water colour.ts
    Tested how to edit the style file while changing the water colour.

--- src/lib/style.ts
+++ src/lib/style.ts
@@ -18,7 +18,7 @@ export const defaultMapConfig: MapConfig = {
       classes: [1, 5, 6]
     },
     water: {
-      color: '#e2e8f0',
+      color: '#4594A3',
       classes: [20, 21, 22]
     },
     cropland: {
